5733d9c15a54a8fc52bc30c2be6916251cc5e569
User X <User X@DESKTOP-K2RICO0>
2022-12-03 22:51:19 +0200

    Add project files.

---
+++ HipermarketApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HipermarketApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
